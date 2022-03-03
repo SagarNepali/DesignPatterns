26fa6ae7c1fc4fa61a63e1122481fd4c7224bfa0
Sagar <worthless.dumboo@gmail.com>
2022-03-03 10:26:44 -0600

    Observer Skeleton

---
+++ src/observerPattern/DAO/AccountDAO.java
@@ -0,0 +1,12 @@
+package observerPattern.DAO;
+
+import observerPattern.domain.Account;
+
+import java.util.Collection;
+
+public interface AccountDAO {
+	void saveAccount(Account account);
+	void updateAccount(Account account);
+	Account loadAccount(String accountnumber);
+	Collection<Account> getAccounts();
+}
